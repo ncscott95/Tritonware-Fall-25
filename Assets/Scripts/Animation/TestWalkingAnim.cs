6ffabdb83f1089690562166018c4d305529eb155
Nathan Scott <122716087+ncscott95@users.noreply.github.com>
2025-10-20 21:34:18 -0700

    Add player swapping legs animations

--- Assets/Scripts/Animation/TestWalkingAnim.cs
+++ Assets/Scripts/Animation/TestWalkingAnim.cs
@@ -1,20 +1,50 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class TestWalkingAnim : MonoBehaviour
 {
+    // public GameObject baseLegsPrefab;
+    // public GameObject enemyLegsPrefab;
+    // public Transform legsParent;
     private Animator animator;
     private bool isWalking = false;
+    public Transform legsObject;
+    private bool usingEnemyLegs = false;
+    // private string legsObjectName;
+
+    // 0 = back upper, 1 = back lower, 2 = front upper, 3 = front lower
+    public List<SpriteRenderer> legsSpriteRenderers;
+    public List<Sprite> baseLegsSprites;
+    public List<Sprite> enemyLegsSprites;
 
     void Start()
     {
         animator = GetComponent<Animator>();
         animator.Play("Idle", 0);
         animator.Play("Idle", 1);
+        // legsObjectName = currentLegsObject.name;
     }
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.W)) isWalking = !isWalking;
         animator.SetBool("isWalking", isWalking);
+
+        if (Input.GetKeyDown(KeyCode.L)) SwitchLegs();
+    }
+
+    private void SwitchLegs()
+    {
+        usingEnemyLegs = !usingEnemyLegs;
+
+        List<Sprite> selectedSprites = usingEnemyLegs ? enemyLegsSprites : baseLegsSprites;
+        for (int i = 0; i < legsSpriteRenderers.Count; i++)
+        {
+            legsSpriteRenderers[i].sprite = selectedSprites[i];
+        }
+
+        legsObject.localScale = new Vector3(usingEnemyLegs ? -1 : 1, 1, 1);
+        animator.SetBool("usingEnemyLegs", usingEnemyLegs);
+        animator.SetTrigger("resetCycle");
     }
 }
